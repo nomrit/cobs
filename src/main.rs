629599773eaac2baba0aade89264ea4313c986a6
nomrit <Ritsu.Nomura@gmail.com>
2020-07-18 10:48:37 +0900

    COBSのencode/decodeの実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::num::Wrapping;
 use std::env;
 
 fn usage(program: &str) {
@@ -8,11 +7,61 @@ fn usage(program: &str) {
     std::process::exit(1);
 }
 
-fn encode(src: & [u8], dst:& mut [u8]) -> usize {
-    for i in 0..src.len() {
-        dst[i] = (Wrapping(src[i])+Wrapping(1u8)).0
+fn encode(src: & [u8], slen: usize) -> Vec<u8> {
+    let mut i = 0 as usize;
+    let mut f = 0 as usize;
+    let mut t = 0 as usize;
+    let mut v:Vec<u8> = Vec::new();
+
+    while i<slen {
+        if src[i]!=0 {
+            t+=1;
+            if (t-f)>=254 {
+                v.push((t-f+1) as u8);
+                v.extend_from_slice(&src[f..t]);
+                f=t;
+            }
+        } else {
+            v.push((t-f+1) as u8);
+            v.extend_from_slice(&src[f..t]);
+            f=i+1; t=i+1;
+        } 
+        i+=1;
     }
-    return src.len()
+    v.push((t-f+1) as u8);
+    v.extend_from_slice(&src[f..t]);
+    return v;
+}
+
+fn decode(src:Vec<u8>) -> Vec<u8> {
+    let mut v:Vec<u8>=Vec::new();
+    let len = src.len();
+    let mut i=0 as usize;
+
+    while i<len {
+        let l = src[i] as usize;
+        if l>1 {
+            v.extend_from_slice(&src[i+1..i+l]);
+            if l<0xFF {
+                v.push(0);
+            }
+        } else {
+            v.push(0);
+        }
+        i+=l;
+    }
+    v.pop();
+    return v;
+}
+
+fn cobstest(src:Vec<u8>) -> bool {
+    let v1 = encode(&src,src.len());
+    println!("{:?}",v1);
+    let v2 = decode(v1);
+    println!("{:?}",v2);
+
+    assert_eq!(src,v2);
+    return true;
 }
 
 fn main() -> io::Result<()> {
@@ -29,20 +78,60 @@ fn main() -> io::Result<()> {
     println!("{}",infile);
     println!("{}",outfile);
 
-    let mut src = [0;256];
-    let mut dst = [0;256];
+    {
+        let src:Vec<u8>=vec![0];
+        println!("*****TEST1*****");
+        println!("SRC={:?}",src);
 
-    for i in 0..256 {
-        src[i] = i as u8;
+        cobstest(src);
     }
+    {
+        let mut src:Vec<u8>=Vec::new();
+
+        for i in 1..0x100 {
+            src.push(i as u8);
+        }
+        println!("*****TEST2*****");
+        println!("SRC={:?}",src);
     
-    let r = encode(&src,&mut dst);
-
-    assert_eq!(r,256);
-
-    for i in 0..256 {
-        println!("i={}, src={:<02x}, dst={:<02x}",i,src[i],dst[i])
+        cobstest(src);
     }
+    {
+        let mut src:Vec<u8>=Vec::new();
 
+        for i in 1..0xff {
+            src.push(i as u8);
+        }
+        println!("*****TEST3*****");
+        println!("SRC={:?}",src);
+    
+        cobstest(src);
+    }
+    {
+        let mut src:Vec<u8> = Vec::new();
+
+        println!("*****TEST4*****");
+        for i in 2..0x100 {
+            src.push(i as u8);
+        }
+        src.push(0 as u8);
+        println!("SRC={:?}",src);
+
+        cobstest(src);
+    }
+    {
+        let src:Vec<u8>=vec![1,0,0,0];
+        println!("*****TEST5*****");
+        println!("SRC={:?}",src);
+
+        cobstest(src);
+    }
+    {
+        let src:Vec<u8>=vec![0,0];
+        println!("*****TEST6*****");
+        println!("SRC={:?}",src);
+
+        cobstest(src);
+    }
     std::process::exit(0);
 }
